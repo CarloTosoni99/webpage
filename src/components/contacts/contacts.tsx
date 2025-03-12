b303a2a2628f9accc5353ee2f84bc246f00d1d8c
CarloTosoni <tosonicarlo99@gmail.com>
2025-03-12 19:17:18 +0100

    fixed CV

--- src/components/contacts/contacts.tsx
+++ src/components/contacts/contacts.tsx
@@ -13,7 +13,7 @@ export default component$(() => {
       <h1 id='cont-title'>My Contacts</h1>
       <div class='box' id='cont-box'>
         <p>If you'd like to contact me to discuss research or for any other reason, feel free to email me at<a class="hyplink" href="mailto:carlo.tosoni@unive.it">carlo.tosoni@unive.it</a>.</p>
-        <p>Otherwise, here you can find my<a class="hyplink" href="../src/media/EnglishCVMarch.pdf" target="_blank">CV</a>.</p>
+        <p>Otherwise, here you can find my<a class="hyplink" href="/media/EnglishCVMarch.pdf" target="_blank">CV</a>.</p>
         <p>I don't use much social media, but here you can find my LinkedIn account</p>
         <div id="cont-div-linkedin">
           <LinkedIn id="linkedInIcon"></LinkedIn><p id="cont-par-linkedin"><a class="hyplink" href={linkedInLink} target="_blank">{linkedInLink}</a></p>
