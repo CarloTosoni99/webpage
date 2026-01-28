6578a5554c193469d1430592947796e9b93356ce
CarloTosoni <tosonicarlo99@gmail.com>
2026-01-28 22:53:31 +0100

    added accordion in talks

--- src/components/talk/talk.tsx
+++ src/components/talk/talk.tsx
@@ -1,4 +1,4 @@
-import { component$, useStyles$ } from '@builder.io/qwik';
+import { component$, useStyles$, useSignal } from '@builder.io/qwik';
 
 import style from './talk.css?inline';
 
@@ -11,21 +11,31 @@ interface articledata {
 export default component$<articledata>((props) => {
   useStyles$(style);
 
+  const isVisible = useSignal(false);
+
   return (
     <div>
       <hr class="sep"/>
-      <div class='box talk'>
-        <div class='talk-left-dim'>
-          <h3 id='talk-header'><span id='talk-title'>Title: </span>{props.data.title}</h3>
-          <p class='talk-section'><span class='talk-content'>Event: </span>{props.data.event}</p>
-          <p class='talk-section'><span class='talk-content'>Venue: </span>{props.data.venue}</p>
-          <p class='talk-section'><span class='talk-content'>Date: </span>{props.data.date}</p>
-          <p class='talk-section'><span class='talk-content'>Description: </span>{props.data.description}</p>
-          <p id='talk-slide-link'><a class="hyplink" id="talk-hyplink" href={props.data.pdflocation} target="_blank">Download here the slides</a></p>
+      <div class="box talk-box">
+        <div id="talk-header-div">
+          <h3>{props.data.title}</h3>
+          <button class={`button talk-header-button ${isVisible.value ? "talk-header-button-clicked" : ""}`} 
+            onClick$={() => isVisible.value = !isVisible.value}>
+            {isVisible.value ? "shrink" : "expand"}
+          </button>
         </div>
-        <object class='talk-right-dim' id='talk-pdf' data={props.data.pdflocation} type="application/pdf" width="100%" height="500px">
-          <p>Unable to preview, <a href="/presentations/SPIRE_2025.pdf">download</a> here instead.</p>
-        </object>
+        <div class={`talk ${isVisible.value ? "talk-show" : "talk-hide"}`}>
+          <div class='talk-left-dim'>  
+            <p class='talk-section'><span class='talk-content'>Event: </span>{props.data.event}</p>
+            <p class='talk-section'><span class='talk-content'>Venue: </span>{props.data.venue}</p>
+            <p class='talk-section'><span class='talk-content'>Date: </span>{props.data.date}</p>
+            <p class='talk-section'><span class='talk-content'>Description: </span>{props.data.description}</p>
+            <p id='talk-slide-link'><a class="hyplink" id="talk-hyplink" href={props.data.pdflocation} target="_blank">Download here the slides</a></p>
+          </div>
+          <object class='talk-right-dim' id='talk-pdf' data={props.data.pdflocation} type="application/pdf" width="100%" height="500px">
+            <p>Unable to preview, <a href="/presentations/SPIRE_2025.pdf">download</a> here instead.</p>
+          </object>
+        </div>
       </div>
     </div>
   );
